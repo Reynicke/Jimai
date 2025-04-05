900bf6a4b1e61b24d8185e662ce0deeb22aac0c9
Reynicke <ndrs.trst@gmail.com>
2025-04-05 14:21:56 +0200

    Improve mapping documentation

--- src/config.example.js
+++ src/config.example.js
@@ -14,30 +14,36 @@ module.exports = {
   },
 
   kimai: {
-    api: 'HTTP://KIMAI/core/json.php',
+    api: 'https://KIMAI/core/json.php',
     auth: ['USER', 'PASSWORD']
   },
 
   mapping: {
+    // Default mapping, if no other match is found.
+    // Set projectId and taskId to the id of the project and task in Kimai.
+    // label is just for you to identify the match in preview.
+    // textPattern defines the Kimai text. Besides literal text, you can use [placeholders] which match Jira fields.
     default: {
       projectId: 1,
       taskId: 1,
-      label: 'SOME TEXT',
+      label: 'DEFAULT WORK',
       textPattern: '[key] [summary]'
     },
+    // Match by jira issue key
     keys: {
       'PROJ-1': {
         projectId: 1,
         taskId: 2,
-        label: 'SOME TEXT',
-        textPattern: '[key]'
+        label: 'WORK ON PROJ-1',
+        textPattern: 'worked on [key]'
       }
     },
+    // Match by jira issue custom field like labels
     customFields: {
       'labels=TEST': {
         projectId: 1,
         taskId: 2,
-        label: 'SOME TEXT',
+        label: 'TESTING WORK',
         textPattern: 'Has labels: [labels]'
       }
     }
